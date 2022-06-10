b47146845b606624919f204ae602ad579467cdc8
Russell Watson <92612217+wats3082@users.noreply.github.com>
2022-06-10 14:53:44 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
+  }
 
+  tick() {
+    this.setState(state => ({
+      seconds: state.seconds + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
 function App() {
   return (
     <div className="App">
