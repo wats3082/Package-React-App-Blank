858cda8653d15ffb972ee1a1febf642a40b5dc5b
Russell Watson <92612217+wats3082@users.noreply.github.com>
2024-03-09 11:49:45 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. this is a test motherfucker
+          Edit <code>src/App.js</code> and save to reload. this is a test mothr
         </p>
         <a
           className="App-link"
